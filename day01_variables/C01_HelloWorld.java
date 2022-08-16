222ed3c66a198513e2672d2e3a52ae048f462c1d
Spring Stone <springbestone@gmail.com>
2022-08-16 11:51:00 +0300

    Ver2

--- day01_variables/C01_HelloWorld.java
+++ day01_variables/C01_HelloWorld.java
@@ -3,5 +3,6 @@ package day01_variables;
 public class C01_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Merhaba Github");
     }
 }
